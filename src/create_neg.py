0d7846b5eea452f9921d637ea1a059f54d1cc6a9
Dylan Zenner <dylan.zenner@arcaneanalytics.io>
2021-11-14 18:39:20 -0600

    added create_neg.py

---
+++ src/create_neg.py
@@ -0,0 +1,11 @@
+import os
+
+
+def generate_negative_description_file():
+    """
+    Creates the required negative description file to be used for the cascade classifier
+    :return: None
+    """
+    with open("neg.txt", "w") as f:
+        for filename in os.listdir("negative_images"):
+            f.write("negative_images/" + filename + "\n")
