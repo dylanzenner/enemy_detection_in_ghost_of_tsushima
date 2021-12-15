e50ff16fec3404d4e5dfc7f791ce8daff6d555fd
Dylan Zenner <dzenner@phantomdata.io>
2021-12-15 17:18:17 -0600

    Update create_neg.py

--- src/create_neg.py
+++ src/create_neg.py
@@ -9,3 +9,6 @@ def generate_negative_description_file():
     with open("neg.txt", "w") as f:
         for filename in os.listdir("negative_images"):
             f.write("negative_images/" + filename + "\n")
+            
+ if __name__ == "__main__":
+    generate_negative_description_file()
